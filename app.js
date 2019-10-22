21ba97148d60f92e6aa2dc91a95c9d607024c4dc
lmz <lmz@shenumlmz.local>
2019-10-22 15:10:12 +0800

    add feature:  1. deploy  2. arrow down/up to select job

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ Vue.component('app', {
         return {
             url: val && val.data || '',
             value: '',
-            jobs: []
+            jobs: [],
+            selectedLi: 0
         };
     },
 
-    template: `<div class="main" v-if="!url">jenkins url:
-        <input v-model="value"/>
-        <button @click="save">保存</button>
-    </div><ul v-else>
-        <li @click="build(job)" v-for="job of jobs">{{job.result}}{{job.name}}</li>
+    template: `<div class="config" v-if="!url">
+        <div class="label">jenkins url:</div>
+        <div class="input">
+            <input placeholder="格式：http://{username}:{passwd}@{hostname}:{port}, example: http://jack:123@aa.com:8080" v-model="value"/>
+        </div>
+        <div class="btn" @click="save">保存</div>
+    </div><ul class="list" ref="list" v-else>
+        <li @click="build(job)" ref="li" v-for="(job, index) of jobs" :class="{selected: selectedLi == index}">
+        <span v-if="job.color" class="c-tag" :class="job.color"></span>
+        <span class="times" v-if="job.time">{{job.time}}</span>
+        {{job.name}}</li>
     </ul>`,
+
+    created() {
+        this.initKeyEvent();
+        if (this.url) {
+            this.initJenkins();
+        }
+        utools.onPluginEnter(({code, type, payload}) => {
+            let args = payload.split(' ')[1] || '';
+            this.initJenkins(args);
+        })
+        utools.onPluginOut(() => {
+            utools.removeSubInput();
+        })
+    },
 
     methods: {
         save() {
@@ -22,22 +43,58 @@ Vue.component('app', {
             } else {
                 utools.db.put({_id: 'url', data: this.value})
                 this.url = utools.db.get('url').data;
-                createJenkins(this.url);
-                utools.setSubInput(async ({text}) => {
-                    await fetchJobs(this.url).then((jobs) => {
-                        this.jobs = jobs.map((job) => {
-                            job.result = {};
-                            return job;
-                        });
-                    })
-                    return this.jobs;
-                }, 'jenkins')
-
+                this.initJenkins();
             }
         },
         build(job) {
-            buildJob(job.name).then((data) => {
-                job.result = data;
+            if (!job.time) {
+                job.time = 0;
+            }
+            job.time++;
+            buildJob(job.name);
+            utools.showNotification(`${job.name} building` );
+        },
+        initJenkins(args) {
+            createJenkins(this.url);
+            fetchJobs(this.url).then((jobs) => {
+                this.jobs = jobs;
+                utools.setSubInput(({text}) => {
+                    this.selectedLi = 0;
+                    if (!text) {
+                        return jobs;
+                    }
+                    this.jobs = jobs.filter((job) => ~job.name.indexOf(text)).sort((a,b) => (b.time || 0) - (a.time|| 0));
+                }, '请输入jenkins任务名');
+                if (args) {
+                    utools.setSubInputValue(args);
+                }
+            })
+
+        },
+
+        initKeyEvent() {
+            $(document).keydown((e) => {
+                let list = this.$refs.list;
+                if (!list) {
+                    return;
+                }
+                switch(e.keyCode){
+                    case 13:
+                        this.build(this.jobs[this.selectedLi]);
+                        break;
+                    case 38: //上
+                        if (this.selectedLi > 0) {
+                            this.selectedLi--;
+                            document.documentElement.scrollTop = this.$refs.li[this.selectedLi].offsetTop;
+                        }
+                        break;
+                    case 40:  //下
+                        if (this.selectedLi < this.$refs.li.length-1) {
+                            this.selectedLi++;
+                            document.documentElement.scrollTop = this.$refs.li[this.selectedLi].offsetTop;
+                        }
+                        break;
+                }
             });
         }
     }
--- preload.js
+++ preload.js
@@ -1,28 +1,57 @@
 const fs = require('fs');
 
 var jenkins;
+const _job_top10 = [];
+const _jobRuningQueue = [];
+var _jobs;
 
 window.createJenkins = (url) => {
-    jenkins = require('jenkins')({ baseUrl: url, crumbIssuer: true });
+    jenkins = require('jenkins')({ baseUrl: url, crumbIssuer: true, promisify: true });
 };
 
 window.fetchJobs = function (url) {
-    return new Promise((resolve, reject) => {
-        jenkins.job.list(function (err, jobs) {
-            if (err) {
-                reject(err);
-            } else if (jobs) {
-                resolve(jobs);
-            }
+    if (_jobs) {
+        return Promise.resolve(_jobs);
+    }
+    return jenkins.job.list().then((jobs) => {
+        _jobs = jobs.sort((a,b) => {
+            let aPrefix = a.name.indexOf('qiyihao'),
+                bPrefix = b.name.indexOf('qiyihao');
+            return ~aPrefix - ~bPrefix;
+        }).map(job => {
+            job.time = 0;
+            return job;
         });
+        return _jobs;
     });
 }
 
 window.buildJob = function(input) {
-    return new Promise((resovle, reject) => {
-        jenkins.job.build(input, (err, data) => {
-            if (err) reject(err);
-            else resovle(data);
-        });
+    // _job_top10
+    return jenkins.job.build(input);
+}
+
+window.addQueue = function(job, id) {
+    let _runingJob = {
+        timestamp: Date.now(),
+        fn: () => new Promise ((resolve, reject) => {
+                jenkins.build.get(job.name, 1, (err, data) => {
+                    if (err) reject(err);
+                    else resolve(job, data);
+                })
+        })
+    };
+    // _jobQueue.push(_runingJob);
+    return _runingJob;
+}
+
+function runJobQueue() {
+    _jobQueue.map((job) => {
+        job.fn().then((job, data) => {
+            job.result = data;
+            job.color = data.color;
+        }).catch(() => {
+
+        })
     });
 }
